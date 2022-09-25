70516a9f38bb5e8921579144e5cf9c8e44735e23
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2022-09-25 04:30:48 -0700

    Organize code into modules

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,52 @@
-fn main() {
-    let get: Method = Method::GET;
-    let post: Method = Method::POST;
-    let delete: Method = Method::DELETE;
-    let put: Method = Method::PUT;
+use http::request::Request;
+use server::Server;
 
+fn main() {
     let addr = String::from("127.0.0.1:8080");
 
     let server = Server::new(addr);
     server.run();
 }
 
-struct Server {
-    addr: String,
-}
-
-enum Method {
-    GET,
-    POST,
-    PUT,
-    DELETE,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH,
-}
-
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: Method,
-}
-
-impl Server {
-    // Associated function
-    // Self is alias for Struct itself
-    fn new(addr: String) -> Self {
-        Self { addr }
+mod server {
+    pub struct Server {
+        addr: String,
     }
 
-    fn run(self) {
-        println!("Running on {} ", self.addr);
+    impl Server {
+        // Associated function
+        // Self is alias for Struct itself
+        pub fn new(addr: String) -> Self {
+            Self { addr }
+        }
+
+        pub fn run(self) {
+            println!("Running on {} ", self.addr);
+        }
+    }
+}
+
+mod http {
+    pub mod method {
+        pub enum Method {
+            GET,
+            POST,
+            PUT,
+            DELETE,
+            HEAD,
+            CONNECT,
+            OPTIONS,
+            TRACE,
+            PATCH,
+        }
+    }
+
+    pub mod request {
+        use super::method::Method;
+        pub struct Request {
+            path: String,
+            query_string: Option<String>,
+            method: Method,
+        }
     }
 }
